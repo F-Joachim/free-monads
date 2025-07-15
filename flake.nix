86b8a3fcc3679606a376602acd64dde5a43c143a
Joachim Friedrich <49515773+F-Joachim@users.noreply.github.com>
2025-07-15 15:14:48 +0200

    Fix error "invalid shell option name" in appearing in VSCode terminal.

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+# flake.nix
 {
   description = "A Haskell project with Free Monads";
 
@@ -9,18 +10,25 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        ghcVersion = "ghc984";
         pkgs = nixpkgs.legacyPackages.${system};
-        haskellPackages = pkgs.haskell.packages.${ghcVersion};
+        haskellPackages = pkgs.haskell.packages.ghc984;
       in
       {
         devShells.default = haskellPackages.developPackage {
           root = ./.;
-          modifier = drv: pkgs.haskell.lib.addBuildTools drv [
-            haskellPackages.haskell-language-server
-            haskellPackages.stack
-          ];
+          modifier = drv: drv.overrideAttrs (old: {
+
+            buildInputs = old.buildInputs ++ (with pkgs; [
+              bashInteractive # Add bashInteractive to avoid "invalid shell option name" in VSCode
+            ]);
+
+            nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs; [
+              haskell-language-server
+              stack
+              git
+              pkg-config
+            ]);
+          });
         };
-      }
-    );
+      });
 }
